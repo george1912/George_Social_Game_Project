781e1a017acd613e5f9a970113d41cf47537ae6a
george1912 <gcu201@nyu.edu>
2014-10-03 16:43:58 -0400

    New game logic and test
    New game logic and test ongoing changes

---
+++ Foxgeeselogictest.js
@@ -0,0 +1,188 @@
+describe("In foxandgeese", function() {
+  function expectMoveOk(turnIndexBeforeMove, stateBeforeMove, move) {
+    expect(isMoveOk({turnIndexBeforeMove: turnIndexBeforeMove,
+      stateBeforeMove: stateBeforeMove,
+      move: move})).toBe(true);
+  }
+
+  function expectIllegalMove(turnIndexBeforeMove, stateBeforeMove, move) {
+    expect(isMoveOk({turnIndexBeforeMove: turnIndexBeforeMove,
+      stateBeforeMove: stateBeforeMove,
+      move: move})).toBe(false);
+  }
+
+
+  it("placing g two places forward is illegal", function() {
+    expectMoveOk(1,
+      {board: 
+    	  [
+			  [B,B,B,g,g,g,B,B,B],
+			  [B,B,B,g,g,g,B,B,B],
+			  [B,B,B,g,g,g,B,B,B],
+			  [g,g,g,g,g,g,g,g,g],
+			  [E,E,E,E,E,E,E,E,E],
+			  [E,E,E,E,E,E,E,E,E]
+			  [B,B,B,E,f,E,B,B,B],
+			  [B,B,B,E,E,E,B,B,B]
+			  [B,B,B,E,E,E,B,B,B]
+
+    	   
+    	   
+    	   ], delta: {row: 6, col: 7}},
+      [{setTurn: {turnIndex : 0}},
+        {set: {key: 'board', value: 
+        	[
+        	 [B,B,B,g,g,g,B,B,B],
+			[B,B,B,g,g,g,B,B,B],
+			[B,B,B,g,g,g,B,B,B],
+			[g,g,g,g,g,g,E,g,g],
+			[E,E,E,E,E,E,E,E,E],
+			[E,E,E,E,f,E,g,E,E]
+			[B,B,B,E,E,E,B,B,B],
+			[B,B,B,E,E,E,B,B,B]
+			[B,B,B,E,E,E,B,B,B]
+        	 
+        	 ]}},
+        {set: {key: 'delta', value: {row: 5, col: 7}}}]);
+  });
+
+  it("f in B illegal placing X in 2x0 is illegal", function() {
+    expectMoveOk(0,
+      {board: [
+               
+               
+					[B,B,B,g,g,g,B,B,B],
+					[B,B,B,g,g,g,B,B,B],
+					[B,B,B,g,g,g,B,B,B],
+					[g,g,g,g,g,g,E,g,g],
+					[E,E,E,E,E,E,E,E,E],
+					[E,E,E,E,f,E,g,E,E]
+					[B,B,B,E,E,E,B,B,B],
+					[B,B,B,E,E,E,B,B,B]
+					[B,B,B,E,E,E,B,B,B]
+					               
+               
+               ],
+               
+               delta: {row: 3, col: 4}},
+      [{endMatch: {endMatchScores: [1, 0]}},
+            {set: {key: 'board', value: 
+            	[
+					            	 
+					[B,B,B,g,g,g,B,B,B],
+					[B,B,B,g,g,g,B,B,B],
+					[B,B,B,g,g,g,B,B,B],
+					[g,g,g,g,g,g,E,g,g],
+					[E,E,E,E,E,E,E,E,E],
+					[E,E,E,E,E,E,g,E,E]
+					[f,B,B,E,E,E,B,B,B],
+					[B,B,B,E,E,E,B,B,B]
+					[B,B,B,E,E,E,B,B,B]
+            	 
+            	 ]}},
+            {set: {key: 'delta', value: {row: 6, col: 0}}}]);
+  });
+
+  it("g in b is illegal", function() {
+    expectMoveOk(1,
+      {board: [
+						               
+						[B,B,B,g,g,g,B,B,B],
+						[B,B,B,g,g,g,B,B,B],
+						[B,B,B,g,g,g,B,B,B],
+						[g,g,g,g,g,g,E,g,g],
+						[E,E,E,E,E,E,E,E,E],
+						[E,E,E,E,f,E,g,E,E]
+						[B,B,B,E,E,E,B,B,B],
+						[B,B,B,E,E,E,B,B,B]
+						[B,B,B,E,E,E,B,B,B]
+               
+               
+               
+               ], 
+               delta: {row: 3, col: 0}},
+      [{endMatch: {endMatchScores: [0, 1]}},
+            {set: {key: 'board', value: 
+            	[
+                                         
+                        [B,B,B,g,g,g,B,B,B],
+						[B,B,B,g,g,g,B,B,B],
+						[B,B,B,g,g,g,B,B,B],
+						[E,g,g,g,g,g,E,g,g],
+						[E,E,E,E,E,E,E,E,E],
+						[E,E,E,E,f,E,g,E,E]
+						[g,B,B,E,E,E,B,B,B],
+						[B,B,B,E,E,E,B,B,B]
+						[B,B,B,E,E,E,B,B,B]
+                                        
+                                        ]}},
+            {set: {key: 'delta', value: {row: 4, col: 1}}}]);
+  });
+
+  
+  
+  it("the ends when less than 6 geese is legal" , function() {
+    expectMoveOk(0,
+      {board: [
+				               
+				[B,B,B,g,g,g,B,B,B],
+				[B,B,B,g,g,g,B,B,B],
+				[B,B,B,g,g,g,B,B,B],
+				[g,g,g,g,g,g,E,g,g],
+				[E,E,E,E,E,E,E,E,E],
+				[E,E,E,E,f,E,E,E,E]
+				[B,B,B,E,E,E,B,B,B],
+				[B,B,B,E,E,E,B,B,B]
+				[B,B,B,E,E,E,B,B,B]
+				
+				]}},
+               
+               delta: {row: 3, col: 4}},
+     
+            {set: {key: 'board', value:
+            	[
+            	 
+            	[B,B,B,E,E,E,B,B,B],
+				[B,B,B,E,E,E,B,B,B],
+				[B,B,B,E,E,E,B,B,B],
+				[E,E,E,g,g,g,E,E,E],
+				[E,E,E,E,E,E,E,E,E],
+				[E,E,E,E,f,E,g,E,E]
+				[B,B,B,E,E,E,B,B,B],
+				[B,B,B,E,E,E,B,B,B]
+				[B,B,B,E,E,E,B,B,B]
+            	 
+            	 ]}},
+            	 {set: {key: 'delta', value: {row: 3, col: 4}}}]);
+  });
+
+  it("null move is illegal", function() {
+    expectIllegalMove(0, {}, null);
+  });
+
+  it("move without board is illegal", function() {
+    expectIllegalMove(0, {}, [{setTurn: {turnIndex : 1}}]);
+  });
+
+  it("move without delta is illegal", function() {
+    expectIllegalMove(0, {}, [{setTurn: {turnIndex : 1}},
+      {set: {key: 'board', value: [
+                                   
+                                   
+                                   
+                   				[B,B,B,g,g,g,B,B,B],
+                   				[B,B,B,g,g,g,B,B,B],
+                   				[B,B,B,g,g,g,B,B,B],
+                   				[g,g,g,g,g,g,E,g,g],
+                   				[E,E,E,E,E,E,E,E,E],
+                   				[E,E,E,E,E,E,E,E,E]
+                   				[B,B,B,E,E,E,B,B,B],
+                   				[B,B,B,E,E,E,B,B,B]
+                   				[B,B,B,E,E,E,B,B,B]
+                                   
+                                   
+                                   
+                                   ]}}]);
+  });
+
+  
