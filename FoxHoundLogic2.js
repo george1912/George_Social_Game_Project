a03c1e62455996257fb19aaff99cce06844e6260
george1912 <gcu201@nyu.edu>
2015-01-13 20:53:40 -0500

    updated code
    updated code

--- FoxHoundLogic2.js
+++ FoxHoundLogic2.js
@@ -1,8 +1,5 @@
-var Fox_Hounds = (function () {
+var isMoveOk = (function () {
 
-    //placing all pieces one step forward to force winning positions to be at risk from the start
-    //this function is supposed to determine the winner for the game
-    //the hound wins if all of the H pieces are located the the last row of the checker board
     function getWinner(row,col,board){
 
         //fox wins if he is located in any of the to right strings this will create a win condition for the fox
@@ -75,36 +72,13 @@ var Fox_Hounds = (function () {
     }
 
 
+    //isEqual Function
+    function isEqual(object1, object2) {
+        return JSON.stringify(object1) === JSON.stringify(object2);
+    }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //function to check if objects are equal
-    //function isEqual(object1, object2) {
-    //return JSON.stringify(object1) === JSON.stringify(object2);
-    //}
-
-
-    //check if a move is legal and doesnt go outside the confines of the board
-    //this needs to be changed as well because we can have a blank space in the col
 
     function checkPosition(row,col,board){
         if(board[row][col] === undefined){
@@ -115,11 +89,6 @@ var Fox_Hounds = (function () {
         }
     }
 
-
-    //movement functions:
-    //Fox and hounds can only move to black squares
-    //Hound can move only forward but either left or right
-    //Fox can move in any direction but I am having trouble putting this in practice this is what I have going now
 
     function isFoxMove(oldrow,oldcol,row,col){
         //this is for the fox moving backwards
@@ -151,24 +120,13 @@ var Fox_Hounds = (function () {
 
 
 
-    //check what the cell contains I didn't end up using it for my game
-    //function isContain(arr,value) {
-    //for(var i=0; i<arr.length; i++){
-    //if(arr[i][0] == value[0] && arr[i][1] == value[1]){
-    //return true;
-    //}
-    //}
-    //return false;
-    //}
-
-
 
 
     function createMove(oldrow,oldcol,row,col,turnIndexBeforeMove,boardBeforeMove,turnIndex){
 
         if(boardBeforeMove === undefined) {
             boardBeforeMove = [
-                ['H','','H','','H','','H',''],
+                ['','H','','H','','H','','H'],
                 ['','','','','','','',''],
                 ['','','','','','','',''],
                 ['','','','','','','',''],
@@ -184,105 +142,69 @@ var Fox_Hounds = (function () {
         if (checkPosition(row,col,boardBeforeMove) === false){  // checkPosition 01 - boundary
             throw new Error("You cannot make a move outside of the board!");
         }
-        if(boardBeforeMove[oldrow][oldcol] === ''){
-            throw new Error("One cannot make a move from an empty position!");
-        }
+
+
+
+        //checking if you can move to the proper direction
         if(boardBeforeMove[row][col] !== ''){
             throw new Error("One can only make a move in an empty position!");
         }
-        if (boardBeforeMove[oldrow][oldcol] === 'F')
-        {
-            if (isFoxMove(oldrow,oldcol,row,col)===true){
-                console.log("single fox movement");
-                return [firstOperation,
-                    {set: {key: 'board', value: boardAfterMove}},
-                    {set: {key: 'delta', value: {oldrow: oldrow, oldcol: oldcol, row: row, col: col}}}];
-            }
-        }
-        else if (boardBeforeMove[oldrow][oldcol] === 'H')
-        {
-            if (isHoundMove(oldrow,oldcol,row,col)===true){
-                console.log("single hound movement");
-                return [firstOperation,
-                    {set: {key: 'board', value: boardAfterMove}},
-                    {set: {key: 'delta', value: {oldrow: oldrow, oldcol: oldcol, row: row, col: col}}}];
-            }
-        }
-        else{
-            console.log("illegal move!");
-            throw new Error("Illegal move!");
-        }
+
+
+
+
         var boardAfterMove = JSON.parse(JSON.stringify(boardBeforeMove));
-        boardAfterMove[row][col] = turnIndexBeforeMove===0?'F' : 'H';	    //Index => 0 than 'F', turnIndex => 1 than 'H'
+
+        boardAfterMove[row][col] = turnIndexBeforeMove===0?'F' : 'H';	    //Index => 0 than 'O', turnIndex => 1 than 'X'
+
         if(boardAfterMove[oldrow][oldcol]===boardAfterMove[row][col]){
             boardAfterMove[oldrow][oldcol] = '';
         }else{
-            throw new Error("Thats not right!");
+            throw new Error("The not the expect move!");
         }
 
-        var winner = getWinner(row,col,boardAfterMove);
+        var winner = getWinner(boardAfterMove);
 
         var firstOperation;
 
         if(winner !== ''){
             firstOperation = {endMatch: {endMatchScores:
                 (winner === 'F' ? [1, 0] : (winner === 'H' ? [0, 1] : [0, 0]))}};
-
-            console.log("player: "+ winner + " WINS!");
+            console.log("player: "+ winner + " WIN!");
         }else{
             firstOperation = {setTurn: {turnIndex: 1 - turnIndexBeforeMove}};
             if(turnIndex !== 1 - turnIndexBeforeMove){
                 throw new Error("current Index doesn't match!");
             }
         }
+
+
+
+        if (isFoxMove(oldrow,oldcol,row,col)===true){
+            console.log("single movement");
+            return [firstOperation,
+                {set: {key: 'board', value: boardAfterMove}},
+                {set: {key: 'delta', value: {oldrow: oldrow, oldcol: oldcol, row: row, col: col}}}];
+        }
+
+
+        if(isHoundMove(oldrow,oldcol,row,col)===true){
+            console.log("multiple movements");
+            return [firstOperation,
+                {set: {key: 'board', value: boardAfterMove}},
+                {set: {key: 'delta', value: {oldrow: oldrow, oldcol: oldcol, row: row, col: col}}}];
+        }
+        else{
+            console.log("illegal move!");
+            throw new Error("Illegal move!");
+        }
+
+
     }
 
 
-    /*
-     function getExampleMoves(initialTurnIndex, initialState, arrayOfRowColSets){
-     var exampleMove = [];
-     var state = initialState;
-     var turnIndex = initialTurnIndex;
-     for(var i=0; i<arrayOfRowColSets.length; i++){
-     var rowColSets = arrayOfRowColSets[i];
-     var move = createMove(rowColSets.oldrow,rowColSets.oldcol,rowColSets.row, rowColSets.col,turnIndex,state.board,1-turnIndex);
-     var stateAfterMove = {board : move[1].set.value, delta : move[2].set.value};
-     exampleMove.push({
-     stateBeforeMove: state,
-     stateAfterMove: stateAfterMove,
-     turnIndexBeforeMove: turnIndex,
-     turnIndexAfterMove: 1 - turnIndex,
-     move: move,
-     comment: {en: rowColSets.comment}
-     });
-     state = stateAfterMove;
-     turnIndex = 1 - turnIndex;
-     }
-     return exampleMove;
-     }
-     */
-
-    /*
-     function getExampleGame(){
-     return getExampleMoves(0, {}, [
-     {oldrow: 6, oldcol: 1, row: 5, col: 0, comment: "Fox makes the first move going forward and too the left"},
-     {oldrow: 1, oldcol: 0, row: 2, col: 1, comment: "Hound1 makes the 2nd move going forward and too the right"},
-     {oldrow: 5, oldcol: 0, row: 4, col: 1, comment: "Fox makes the 3rd move going forward and too the right"},
-     {oldrow: 1, oldcol: 2, row: 2, col: 3, comment: "Hound2 makes the 4th move going forward and too the right"},
-     {oldrow: 4, oldcol: 1, row: 3, col: 0, comment: "Fox makes the 5th move going forward and too the left"},
-     {oldrow: 2, oldcol: 1, row: 3, col: 2, comment: "Hound1 makes the 6th move going forward and too the left"},
-     {oldrow: 3, oldcol: 0, row: 2, col: 1, comment: "Fox makes the 7thth move going forward and too the left"},
-     {oldrow: 1, oldcol: 4, row: 2, col: 5, comment: "Hound3 makes the 8th move going forward and too the right"},
-     {oldrow: 2, oldcol: 1, row: 1, col: 0, comment: "Fox makes the 9th move going forward and too the right"},
-     {oldrow: 1, oldcol: 6, row: 2, col: 7, comment: "Hound4 makes the 10th move going forward and too the right"},
-     {oldrow: 1, oldcol: 0, row: 1, col: 0, comment: "Fox makes the 9th move going forward and too the right winning the game"}
-     ]);
-     }*/
-
-
-
-
     function isMoveOk(params){
+
         try{
             var move = params.move;
             var turnIndexBeforeMove = params.turnIndexBeforeMove;
@@ -298,48 +220,117 @@ var Fox_Hounds = (function () {
             var boardAfterMove = move[1].set.value;
 
             var expectedMove = createMove(oldrow,oldcol,row,col,turnIndexBeforeMove,boardBeforeMove,turnIndex);
-
-            if(!isEqual(move[0], expectedMove[0]) || !isEqual(move[1], expectedMove[1])){
+            if(!isEqual(move[1], expectedMove[1]) || !isEqual(move[2], expectedMove[2])){
                 return false;
             }
-
         } catch(e) {
             return false;
         }
         return true;
     }
 
-
+    //this should return true
     console.log(
         isMoveOk({turnIndexBeforeMove: 0, stateBeforeMove: {},
             move: [{setTurn: {turnIndex : 1}},
                 {set: {key: 'board', value: [
-
                     ['','H','','H','','H','','H'],
                     ['','','','','','','',''],
                     ['','','','','','','',''],
                     ['','','','','','','',''],
                     ['','','','','','','',''],
                     ['','','','','','','',''],
-                    ['','','','','','','',''],
-                    ['F','','','','','','','']
-
+                    ['','F','','','','','',''],
+                    ['','','','','','','','']
                 ]}},
                 {set: {key: 'delta', value: {oldrow: 7, oldcol: 0, row: 6, col: 1}}}]}) );
 
 
 
+    console.log(
+        isMoveOk({turnIndexBeforeMove: 0, stateBeforeMove: {
+            board:[
+                ['','H','','H','','H','','H'],
+                ['','','','','','','',''],
+                ['','','','','','','',''],
+                ['','','','','','','',''],
+                ['','','','','','','',''],
+                ['','','','','','','',''],
+                ['','F','','','','','',''],
+                ['','','','','','','','']
+            ],
+            delta: {oldrow: 7, oldcol: 0, row: 6, col: 1}
+        },
+            move: [{setTurn: {turnIndex : 1}},
+                {set: {key: 'board', value: [
+                    ['','','','H','','H','','H'],
+                    ['H','','','','','','',''],
+                    ['','','','','','','',''],
+                    ['','','','','','','',''],
+                    ['','','','','','','',''],
+                    ['','','','','','','',''],
+                    ['','F','','','','','',''],
+                    ['','','','','','','','']
+                ]}},
+                {set: {key: 'delta', value: {oldrow: 0, oldcol: 1, row: 1, col: 0}}}]}) );
+
+    console.log(
+        isMoveOk({turnIndexBeforeMove: 0, stateBeforeMove: {
+            board:[
+                ['','','','H','','H','','H'],
+                ['','','H','','','','',''],
+                ['','','','','','','',''],
+                ['','','','','','','',''],
+                ['','','','','','','',''],
+                ['F','','','','','','',''],
+                ['','','','','','','',''],
+                ['','','','','','','','']
+            ],
+            delta: {oldrow: 6, oldcol: 1, row: 5, col: 0}
+        },
+            move: [{setTurn: {turnIndex : 1}},
+                {set: {key: 'board', value: [
+                    ['','','','H','','H','','H'],
+                    ['','','','','','','',''],
+                    ['','','','H','','','',''],
+                    ['','','','','','','',''],
+                    ['','','','','','','',''],
+                    ['F','','','','','','',''],
+                    ['','','','','','','',''],
+                    ['','','','','','','','']
+                ]}},
+                {set: {key: 'delta', value: {oldrow: 1, oldcol: 2, row: 2, col: 3}}}]}) );
 
 
 
+    console.log(
+        isMoveOk({turnIndexBeforeMove: 1, stateBeforeMove: {
+            board:[
+                ['','','','','','','',''],
+                ['','F','H','H','','H','',''],
+                ['','','','','','','','H'],
+                ['','','','','','','',''],
+                ['','','','','','','',''],
+                ['','','','','','','',''],
+                ['','','','','','','',''],
+                ['','','','','','','','']
+            ],
+            delta: {oldrow: 1, oldcol: 7, row: 2, col: 6}
+        },
+            move: [{setTurn: {turnIndex : 0}},
+                {set: {key: 'board', value: [
+                    ['','','','','','','',''],
+                    ['','F','H','H','','H','',''],
+                    ['','','','','','','','H'],
+                    ['','','','','','','',''],
+                    ['','','','','','','',''],
+                    ['','','','','','','',''],
+                    ['','','','','','','',''],
+                    ['','','','','','','','']
+                ]}},
+                {set: {key: 'delta', value: {oldrow: 1, oldcol: 1, row: 0, col: 2}}}]}) );
 
-
-
-//return isMoveOk;
-    //return {isMoveOk: isMoveOk, getExampleGame: getExampleGame};
-
-//})();
+    // expected output: ture(single), ture(multi), false(illegal move), ture(X WIN)
 
     return isMoveOk;
 })();
-
