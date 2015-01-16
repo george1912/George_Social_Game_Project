586695898741bd5d99326d08f38a2282649de843
george1912 <gcu201@nyu.edu>
2015-01-16 03:10:35 -0500

    updated
    updated

--- game.js
+++ game.js
@@ -1,56 +1,72 @@
 'use strict';
 
-// TODO: remove stateService before launching the game.
 angular.module('myApp',
     ['myApp.messageService', 'myApp.gameLogic', 'platformApp'])
-  .controller('Ctrl', function (
-      $window, $scope, $log,
-      messageService, stateService, gameLogic) {
+    .controller('Ctrl', function (
+        $window, $scope, $log,
+        messageService, stateService, gameLogic) {
 
-    function updateUI(params) {
-      $scope.jsonState = angular.toJson(params.stateAfterMove, true);
-      $scope.board = params.stateAfterMove.board;
-      if ($scope.board === undefined) {
-        $scope.board = [['', '', ''],
-                        ['', '', ''],
-                        ['', '', '']];
-      }
-    }
-    updateUI({stateAfterMove: {}});
-    var game = {
-      gameDeveloperEmail: "yoav.zibin@gmail.com",
-      minNumberOfPlayers: 2,
-      maxNumberOfPlayers: 2,
-      exampleGame: gameLogic.getExampleGame(),
-      riddles: gameLogic.getRiddles()
-    };
+//how do I set this to a board?
 
-    var isLocalTesting = $window.location.origin === "file://";
-    $scope.move = "[{setTurn: {turnIndex: 1}}, {set: {key: 'board', value:[['X', '', ''], ['', '', ''], ['', '', '']]}}, {set: {key: 'delta', value: {row: 0, col: 0}}}]";
-    $scope.makeMove = function () {
-      $log.info(["Making move:", $scope.move]);
-      var moveObj = eval($scope.move);
-      if (isLocalTesting) {
-        stateService.makeMove(moveObj);
-      } else {
-        messageService.sendMessage({makeMove: moveObj});
-      }
-    };
-
-    if (isLocalTesting) {
-      game.isMoveOk = gameLogic.isMoveOk;
-      game.updateUI = updateUI;
-      stateService.setGame(game);
-    } else {
-      messageService.addMessageListener(function (message) {
-        if (message.isMoveOk !== undefined) {
-          var isMoveOkResult = gameLogic.isMoveOk(message.isMoveOk);
-          messageService.sendMessage({isMoveOkResult: isMoveOkResult});
-        } else if (message.updateUI !== undefined) {
-          updateUI(message.updateUI);
+        function updateUI(params) {
+            $scope.jsonState = angular.toJson(params.stateAfterMove, true);
+            $scope.board = params.stateAfterMove.board;
+            if ($scope.board === undefined) {
+                $scope.board = [
+                    ['','H','','H','','H','','H'],
+                    ['','','','','','','',''],
+                    ['','','','','','','',''],
+                    ['','','','','','','',''],
+                    ['','','','','','','',''],
+                    ['','','','','','','',''],
+                    ['','','','','','','',''],
+                    ['F','','','','','','','']
+                ];
+            }
         }
-      });
+        updateUI({stateAfterMove: {}});
+        var game = {
+            gameDeveloperEmail: "george.ulloa1990@gmail.com",
+            minNumberOfPlayers: 2,
+            maxNumberOfPlayers: 2,
+            exampleGame: gameLogic.getExampleGame()
 
-      messageService.sendMessage({gameReady : game});
-    }
-  });
+        };
+
+        var isLocalTesting = $window.location.origin === "file://";
+        $scope.move = JSON.stringify([{setTurn: {turnIndex: 1}}, {set: {key: 'board', value:[
+            ['','H','','H','','H','','H'],
+            ['','','','','','','',''],
+            ['','','','','','','',''],
+            ['','','','','','','',''],
+            ['','','','','','','',''],
+            ['','','','','','','',''],
+            ['','F','','','','','',''],
+            ['','','','','','','','']]}}, {set: {key: 'delta', value: {oldrow: 7, oldcol: 0, row: 6, col: 1}}}]);
+        $scope.makeMove = function () {
+            $log.info(["Making move:", $scope.move]);
+            var moveObj = eval($scope.move);
+            if (isLocalTesting) {
+                stateService.makeMove(moveObj);
+            } else {
+                messageService.sendMessage({makeMove: moveObj});
+            }
+        };
+
+        if (isLocalTesting) {
+            game.isMoveOk = gameLogic.isMoveOk;
+            game.updateUI = updateUI;
+            stateService.setGame(game);
+        } else {
+            messageService.addMessageListener(function (message) {
+                if (message.isMoveOk !== undefined) {
+                    var isMoveOkResult = gameLogic.isMoveOk(message.isMoveOk);
+                    messageService.sendMessage({isMoveOkResult: isMoveOkResult});
+                } else if (message.updateUI !== undefined) {
+                    updateUI(message.updateUI);
+                }
+            });
+
+            messageService.sendMessage({gameReady : game});
+        }
+    });
--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config){
       // source files, that you wanna generate coverage for
       // do not include tests or libraries
       // (these files will be instrumented by Istanbul)
-      //'gameLogic.js': ['coverage']
+      'gameLogic.js': ['coverage']
     },
 
     // optionally, configure the reporter
